4d77f29195598c651343cc1a86bd355e1fb77384
Anubha Joshi <anubhajoshi01@gmail.com>
2020-10-03 10:35:30 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'app.dart';
+
+void main() async{
+
+
+  runApp(MyApp());
+}
+
