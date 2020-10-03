97601d684d0df5eafe82c26c578a41fcfc247dfd
Mason <lolthetoll@gmail.com>
2020-10-03 15:24:11 -0400

    changes

--- lib/screens/store_info_page.dart
+++ lib/screens/store_info_page.dart
@@ -14,10 +14,50 @@ class StoreInfoPage extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: Text("${store.name}"),
+        centerTitle: true,
+        backgroundColor: Colors.red[700],
+
       ),
-      body: Container()
+      body: Column(
+        crossAxialAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: ExactAssetImage("assets"),
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            Container(
+            padding: EdgeInsets.all(10),
+            child: DefaultTextStyle(
+              child:Container(
+                child: Text("${store.address}"),
+                style:TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: DefaultTextStyle(
+              child: Container(
+                child: Text("${store.restrictions}"),
+                style:TextStyle(
+                  fontSize: 50,
+                  letterSpacing:3,
+                ),
+              ),
+            ),
+          ),
+      ],
+      ),
     );
   }
-
-
 }
